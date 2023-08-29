4d09f20144513d975044f72df3df3b836ede8342
Mike Stall <jmstall@microsoft.com>
2023-08-29 16:51:50 -0700

    add test project

--- SurveyTests/Class1.cs
+++ SurveyTests/Class1.cs
@@ -1,7 +1,14 @@
-﻿namespace SurveyTests
+﻿using Xunit;
+
+namespace SurveyTests
 {
     public class Class1
     {
 
+        [Fact]
+        public void Test()
+        {
+            // Pass
+        }
     }
 }
